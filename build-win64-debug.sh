7013522b7b7d1dfc73b27f6c25008a4a316fdb94
saltycake <aainook@gmail.com>
2018-02-21 18:15:46 +0200

    Win64 cross-compilatino works. Changed some filenames.

--- build-win64-debug.sh
+++ build-win64-debug.sh
@@ -2,9 +2,9 @@
 
 sources="src/main.c.precaked"
 search_dirs="-Iinclude/ -Llib/win64/"
-libs="-lmingw32 -lpthread -lm -lSDL2main -lSDL2 -lopengl32"
-platform="--target=x86_64-w64-mingw32 -mwindows -m64"
+libs="-lmingw32 -lSDL2main -lSDL2"
+platform="-mwindows -m64"
 
 ./precake src/main.c src/stringstuff.h
-clang-5.0 -x c -std=c11 -ferror-limit=1 -Wall -g -O0 $platform \
+x86_64-w64-mingw32-gcc $platform -x c -std=c11 -fmax-errors=1 -Wall -g -O0 \
   -o bin/win64/harcon64-debug.exe $search_dirs $sources $libs
